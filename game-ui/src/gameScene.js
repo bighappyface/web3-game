04ba939a10e78ef99b10a4d99d971bbf603a387f
bighappyface <anomalous20@gmail.com>
2022-02-08 23:10:16 -0500

    Track stars in backend

--- game-ui/src/gameScene.js
+++ game-ui/src/gameScene.js
@@ -6,23 +6,22 @@ import dudeImg from './assets/dude.png'
 import axios from 'axios'
 
 const eventsCenter = new Phaser.Events.EventEmitter()
+const backendURI = 'http://127.0.0.1:6868/api/players/'
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: 'gameScene' })
   }
 
-  updateStarCount(total) {
-    this.starCount = total
-    this.starText.setText('stars: ' + this.starCount)
-  }
-
   init(data) {
     this.userActions = []
+    this.platforms
     this.starCount = 0
+    this.starCountLevel = 0
     this.starText
     this.player
     this.account = data.account
+    this.playerUrl = backendURI + this.account
   }
 
   preload() {
@@ -41,13 +40,13 @@ class GameScene extends Phaser.Scene {
     this.add.image(400, 300, 'sky')
 
     // Platforms
-    const platforms = this.physics.add.staticGroup()
+    this.platforms = this.physics.add.staticGroup()
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody()
 
-    platforms.create(600, 400, 'ground')
-    platforms.create(50, 250, 'ground')
-    platforms.create(750, 220, 'ground')
+    this.platforms.create(600, 400, 'ground')
+    this.platforms.create(50, 250, 'ground')
+    this.platforms.create(750, 220, 'ground')
 
     // Player
     this.player = this.physics.add.sprite(100, 450, 'dude')
@@ -75,28 +74,9 @@ class GameScene extends Phaser.Scene {
       repeat: -1,
     })
 
-    this.physics.add.collider(this.player, platforms)
+    this.physics.add.collider(this.player, this.platforms)
 
-    // Stars
-    const stars = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    })
-
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
-    })
-
-    this.physics.add.collider(stars, platforms)
-
-    function collectStar(player, star) {
-      star.disableBody(true, true)
-      this.starCount++
-      this.starText.setText('stars: ' + this.starCount)
-    }
-
-    this.physics.add.overlap(this.player, stars, collectStar, null, this)
+    this.generateStars()
 
     // Star Text
     this.starText = this.add.text(100, 16, 'stars: ' + this.starCount, {
@@ -113,9 +93,12 @@ class GameScene extends Phaser.Scene {
     // Load Player Stars
     eventsCenter.on('loadStars', this.updateStarCount, this)
 
-    let url = 'http://127.0.0.1:6868/api/players/' + this.account
-    axios.get(url).then((response) => {
+    axios.get(this.playerUrl).then((response) => {
       eventsCenter.emit('loadStars', response.data.stars)
+    })
+
+    this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
+      eventsCenter.off('loadStars', this.updateStarCount, this)
     })
   }
 
@@ -141,12 +124,43 @@ class GameScene extends Phaser.Scene {
     }
 
     // End
-    if (12 == this.starCount) {
-      this.scene.restart()
+    if (this.starCountLevel == 12) {
+      this.starCountLevel = 0
+      this.generateStars()
     }
   }
 
   end() {}
+
+  updateStarCount(total) {
+    this.starCount = total
+    this.starText.setText('stars: ' + this.starCount)
+  }
+
+  generateStars() {
+    // Stars
+    const stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    })
+
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+    })
+
+    this.physics.add.collider(stars, this.platforms)
+
+    function collectStar(player, star) {
+      star.disableBody(true, true)
+      this.starCount++
+      this.starCountLevel++
+      this.starText.setText('stars: ' + this.starCount)
+      axios.put(this.playerUrl, { address: this.account })
+    }
+
+    this.physics.add.overlap(this.player, stars, collectStar, null, this)
+  }
 }
 
 export default GameScene
